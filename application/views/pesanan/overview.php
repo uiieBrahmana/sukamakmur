795fb71b408da4776e81f9e248d48e2c827347ac
Velliz <diditvelliz@outlook.com>
2016-04-26 00:18:17 +0700

    bugfix clear empty order 2

--- application/views/pesanan/overview.php
+++ application/views/pesanan/overview.php
@@ -242,14 +242,14 @@
                                 <input type="hidden" name="URL" value="http://rcsukamakmur.co.id/sukamakmur">
                                 <input type="hidden" name="WORDS"
                                        value="<?php echo securedHash($id, $Total . '.00'); ?>">
-                                <input type="hidden" name="CNAME" value="<?php $Tamu['nama'] ?>">
-                                <input type="hidden" name="CEMAIL" value="<?php $Tamu['email'] ?>">
+                                <input type="hidden" name="CNAME" value="<?php echo $Tamu['nama'] ?>">
+                                <input type="hidden" name="CEMAIL" value="<?php echo $Tamu['email'] ?>">
                                 <input type="hidden" name="CWPHONE"
-                                       value="<?php str_replace('-', '', $Tamu['notelp']) ?>">
+                                       value="<?php echo str_replace('-', '', $Tamu['notelp']) ?>">
                                 <input type="hidden" name="CHPHONE"
-                                       value="<?php str_replace('-', '', $Tamu['notelp']) ?>">
+                                       value="<?php echo str_replace('-', '', $Tamu['notelp']) ?>">
                                 <input type="hidden" name="CMPHONE"
-                                       value="<?php str_replace('-', '', $Tamu['notelp']) ?>">
+                                       value="<?php echo str_replace('-', '', $Tamu['notelp']) ?>">
                                 <input name="submit" type="submit" class="btn btn-primary btn-block"
                                        value="Bayar dengan DOKU WALLET"/>
                             </FORM>
